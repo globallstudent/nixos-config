ab1d43629d5d2f65f5333139945a2f69bab6e58d
Asliddin <asliddinabdumannonov06@gmail.com>
2025-07-06 18:14:40 +0500

    fine tune

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -21,19 +21,28 @@
   programs.zsh.enable = true;
 
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.gnome.core-utilities.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  services.gnome.gnome-browser-connector.enable = true; # required for extensions.gnome.org
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
+
 
   networking.networkmanager.enable = true;
   services.openssh.enable = true;
 
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub = {
     enable = true;
-    device = "/dev/nvme0n1";  # 🔁 adjust if needed (e.g., /dev/nvme0n1)
+    efiSupport = true;
     useOSProber = true;
+    devices = [ "nodev" ];  # ← this means "no block device", use EFI
   };
+  boot.loader.efi.canTouchEfiVariables = true;
+
+
 
   environment.systemPackages = with pkgs; [
     git curl wget zsh
