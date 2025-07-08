27d056c905b5b7a07b03064c7900fe9388903f3e
Asliddin <asliddinabdumannonov06@gmail.com>
2025-07-08 17:21:28 +0500

    programs config

--- users/yunus/home.nix
+++ users/yunus/home.nix
@@ -65,11 +65,6 @@
     userEmail = "asliddinabdumannonov06@gmail.com";
   };
 
-  # Import program configurations
-  imports = [
-    ../../modules/programs
-  ];
-
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
@@ -82,60 +77,6 @@
       golang.go
       rust-lang.rust-analyzer
     ];
-  };
-        history = 10000;
-        multiplier = 3;
-      };
-      font = {
-        normal = {
-          family = "JetBrainsMono Nerd Font";
-          style = "Regular";
-        };
-        bold = {
-          family = "JetBrainsMono Nerd Font";
-          style = "Bold";
-        };
-        italic = {
-          family = "JetBrainsMono Nerd Font";
-          style = "Italic";
-        };
-        size = 11;
-      };
-      colors = {
-        primary = {
-          background = "#1a1b26";
-          foreground = "#c0caf5";
-        };
-        normal = {
-          black = "#15161e";
-          red = "#f7768e";
-          green = "#9ece6a";
-          yellow = "#e0af68";
-          blue = "#7aa2f7";
-          magenta = "#bb9af7";
-          cyan = "#7dcfff";
-          white = "#a9b1d6";
-        };
-        bright = {
-          black = "#414868";
-          red = "#f7768e";
-          green = "#9ece6a";
-          yellow = "#e0af68";
-          blue = "#7aa2f7";
-          magenta = "#bb9af7";
-          cyan = "#7dcfff";
-          white = "#c0caf5";
-        };
-      };
-      cursor = {
-        style = {
-          shape = "Beam";
-          blinking = "On";
-        };
-        blink_interval = 750;
-        unfocused_hollow = true;
-      };
-    };
   };
 
   gtk = {
